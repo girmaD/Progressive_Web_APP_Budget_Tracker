54867c270d0fc20cfb0479af708f2504dd834601
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-20 13:43:55 -0800

    adjusts file paths for the files used

---
+++ public/webpack.config.js
@@ -0,0 +1,46 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: './assets/js/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'Budget Tracker',
+      short_name: 'Budget',
+      description: 'An application tracks your budget both online and offline.',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      'theme-color': '#ffffff',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('assets/images/icons/icon-512x512.png'),
+          sizes: [192, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
