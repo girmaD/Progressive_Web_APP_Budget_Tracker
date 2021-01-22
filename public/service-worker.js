e710b16a1617af31ab10cdbbd1faa5548ede6438
Girma Ebssa <girma.ebssa@gmail.com>
2021-01-22 11:58:01 -0800

    edits service worker and files to be cached

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,59 +1,81 @@
 const FILES_TO_CACHE = [
     '/',
-    '/index.html',    
-    'assets/css/style.css',
-    'assets/js/index.js',    
-    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'    
+    '/index.html',  
+    '/assets/images/icons/icon-192x192.png', 
+    '/assets/images/icons/icon-512x512.png', 
+    'assets/js/index.js',
+    'assets/js/indexDB.js',
+    '/manifest.webmanifest', 
+    '/assets/css/styles.css'             
 ];
   
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "runtime-cache";
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-});
-  
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-        caches
-        .keys()
-        .then((cacheNames) => {
-            return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-            return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-                return caches.delete(cacheToDelete);
-            })
-            );
-        })
-        .then(() => self.clients.claim())
-    );
+// install
+self.addEventListener("install", function (evt) {
+  // pre cache image data
+  evt.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
+    
+  // pre cache all static assets
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-        event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            if (cachedResponse) {
-            return cachedResponse;
+// activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
             }
 
-            return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-                return cache.put(event.request, response.clone()).then(() => {
-                return response;
-                });
-            });
-            });
-        })
-        );
-    }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
 });
